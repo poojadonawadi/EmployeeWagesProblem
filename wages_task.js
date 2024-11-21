1797856fb2e7f36cae84b8bc1e4ed1859c476bfb
poojadonawadi <138244294+poojadonawadi@users.noreply.github.com>
2024-11-21 22:11:26 +0530

    [Pooja] added attendance

--- wages_task.js
+++ wages_task.js
@@ -1,2 +1,8 @@
 //Welcome Employee
 console.log("Welcome to employee wage computation program");
+
+// Attendance checking
+function checkAttendance() {
+    const attendance = Math.random();
+    return attendance > 0.2 ? 1 : 0; // 1 indicates present, 0 indicates absent
+}
