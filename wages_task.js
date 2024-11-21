e98249f1ff9547b18aad9de94640c96500f2faad
poojadonawadi <138244294+poojadonawadi@users.noreply.github.com>
2024-11-21 22:19:37 +0530

    [Pooja] daily wages

--- wages_task.js
+++ wages_task.js
@@ -6,3 +6,8 @@ function checkAttendance() {
     const attendance = Math.random();
     return attendance > 0.2 ? 1 : 0; // 1 indicates present, 0 indicates absent
 }
+
+// Calculation of daily wages
+function dailyWages(wagesPerHour, numberOfHours) {
+    return wagesPerHour * numberOfHours;
+}
