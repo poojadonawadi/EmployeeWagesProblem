2c7f86d53337239293627c0bc680fe36d3f434b1
poojadonawadi <138244294+poojadonawadi@users.noreply.github.com>
2024-11-21 22:23:35 +0530

    [Pooja] added switch case

--- wages_task.js
+++ wages_task.js
@@ -11,3 +11,25 @@ function checkAttendance() {
 function dailyWages(wagesPerHour, numberOfHours) {
     return wagesPerHour * numberOfHours;
 }
+
+// Switch case for work hours
+function getWorkHours(employeeType) {
+    const hoursWorked = [0, 4, 8]; // Absent (0), Part-Time (4), Full-Time (8)
+    let workHours = 0;
+
+    switch (employeeType) {
+        case 0: // Absent
+            workHours = hoursWorked[0];
+            break;
+        case 1: // Part-Time
+            workHours = hoursWorked[1];
+            break;
+        case 2: // Full-Time
+            workHours = hoursWorked[2];
+            break;
+        default:
+            console.log("Invalid employee type");
+            break;
+    }
+    return workHours;
+}
